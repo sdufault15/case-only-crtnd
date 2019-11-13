3f9d70d82d198b88d9f9771dbfc9520b72132887
Suzanne Dufault <sdufault@Biostat-MBP13-21.local>
2019-11-12 17:22:20 -0800

    parallelizing and adding the legacy nest function

--- lib/multinom_functions/performance-evaluation-function.R
+++ lib/multinom_functions/performance-evaluation-function.R
@@ -1,16 +1,16 @@
 performance_function <- function(df, m, zstar, estimator = c("AggOR", "ME.GEE", "TP")){
   
-  df_temp <- as_tibble(df) %>% nest(-PERIOD, -iter, -lambda, -hcsb, -size)
+  df_temp <- as_tibble(df) %>% nest_legacy(-PERIOD, -iter, -lambda, -hcsb, -size)
   
   if("AggOR" %in% estimator){
-    df_temp <- df_temp %>% mutate(AggOR = map(data, ~aggregate_or_function(., m, zstar))) %>% 
+    df_temp <- df_temp %>% mutate(AggOR = future_map(data, ~aggregate_or_function(., m, zstar))) %>% 
       mutate(OR.agg = map_dbl(AggOR, 1),
              sd.agg = map_dbl(AggOR, 2),
              sig.agg = map_dbl(AggOR, 3))
   }
   
   if("ME.GEE" %in% estimator){
-    df_temp <- df_temp %>% mutate(ME.GEE = map(data, ~mixed_effects_function(.))) %>%
+    df_temp <- df_temp %>% mutate(ME.GEE = future_map(data, ~mixed_effects_function(.))) %>%
       mutate(OR.gee = map_dbl(ME.GEE, 1),
              sd.gee = map_dbl(ME.GEE, 2),
              pval.gee = map_dbl(ME.GEE, 3),
@@ -20,7 +20,7 @@ performance_function <- function(df, m, zstar, estimator = c("AggOR", "ME.GEE", 
   }
   
   if("TP" %in% estimator){
-    df_temp <- df_temp %>% mutate(TP = map(data, ~test_positive_function(., m = m, zstar = zstar))) %>%
+    df_temp <- df_temp %>% mutate(TP = future_map(data, ~test_positive_function(., m = m, zstar = zstar))) %>%
       mutate(OR.tp = map_dbl(TP, 1),
              sd.tp = map_dbl(TP, 2),
              pval.tp = map_dbl(TP, 3))
